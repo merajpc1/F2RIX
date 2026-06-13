3ccc2698eb0cb6989e75aa8f87b9d61e009f3ef4
merajpc1 <merajpc1@gmail.com>
2026-06-13 16:53:07 +0600

    Update random.js

--- Script/commands/random.js
+++ Script/commands/random.js
@@ -26,7 +26,7 @@ module.exports.run = async function ({ api, event }) {
  
  let sendVideo = function () {
  api.sendMessage({
- 'body': "𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 \nAdded by: [" + videoName + "]\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:" + videoCount + "...🎬\n\n｢─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭｣",
+ 'body': "𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 \nAdded by: [" + videoName + "]\n𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:" + videoCount + "...🎬\n\n｢─꯭─⃝‌‌F2RIX 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭｣",
  'attachment': fs.createReadStream(__dirname + "/cache/Shaoon.mp4")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/Shaoon.mp4"), event.messageID);
  };
